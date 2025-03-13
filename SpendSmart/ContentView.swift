6c2becbf0407cb6f1867f972e9c9522ec2b8e1c7
Shaurya <shaurya50211@gmail.com>
2025-03-12 21:27:24 -0400

    first commit

---
+++ SpendSmart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendSmart
+//
+//  Created by Shaurya Gupta on 2025-03-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendSmart/SpendSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSmartApp.swift
+//  SpendSmart
+//
+//  Created by Shaurya Gupta on 2025-03-12.
+//
+
+import SwiftUI
+
+@main
+struct SpendSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
